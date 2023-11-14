caf588e85d5d0b709a5e2a695221be00488f56d4
Okan Tasin(ETIYA) <okan.tasin@etiya.com>
2023-11-14 22:20:02 +0300

    feat(UI-01): add menu , user component and user service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { UserComponent } from './component/user/user.component';
+import { HomeComponent } from './component/home/home.component';
+import { MenuComponent } from './menu/menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserComponent,
+    HomeComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule
